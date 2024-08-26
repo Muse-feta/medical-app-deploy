4bdee8b0e5509e6c61f1c1fc97a46fa59d98bd43
Muse-feta <fetamuse@gmail.com>
2024-08-26 11:43:41 +0300

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["api.dicebear.com"], // Add any other domains you need
+  },
+  // Other configurations...
+};
+
+module.exports = nextConfig;
