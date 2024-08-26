9e7bfe70765e06bc38cbef39313990b445aed492
Muse-feta <fetamuse@gmail.com>
2024-08-26 12:49:17 +0300

    minor changes

--- app/api/card-info/route.ts
+++ app/api/card-info/route.ts
@@ -4,38 +4,36 @@ import { PrismaClient, status } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const GET = async () => {
-    try {
-        // first get all the users
-        const users = await prisma.user.findMany();
+  try {
+    // first get all the users
+    const users = await prisma.user.findMany();
 
-        // get new users in the last 30 days
-        const newUsersLast30Days = users.filter((user) => {
-            const createdAt = new Date(user.createdAt);
-            const thirtyDaysAgo = new Date();
-            thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
-            return createdAt > thirtyDaysAgo;
-        });
+    // get new users in the last 30 days
+    const newUsersLast30Days = users.filter((user) => {
+      const createdAt = new Date(user.createdAt);
+      const thirtyDaysAgo = new Date();
+      thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+      return createdAt > thirtyDaysAgo;
+    });
 
-        // get all appointments
-        const appointments = await prisma.appointment.findMany();
+    // get all appointments
+    const appointments = await prisma.appointment.findMany();
 
-        // get all active appointments
-        const activeAppointments = appointments.filter((appointment) => {
-            return status[appointment.status] === "PENDING";
-        });
+    // get all active appointments
+    const activeAppointments = appointments.filter((appointment) => {
+      return status[appointment.status] === "PENDING";
+    });
 
-        const cardInfo = {
-            totalUsers: users.length,
-            newUsersLast30Days: newUsersLast30Days.length,
-            totalAppointments: appointments.length,
-            activeAppointments: activeAppointments.length,
-        };
+    const cardInfo = {
+      totalUsers: users.length,
+      newUsersLast30Days: newUsersLast30Days.length,
+      totalAppointments: appointments.length,
+      activeAppointments: activeAppointments.length,
+    };
 
-        return NextResponse.json(cardInfo);
-        
-        
-    } catch (error: any) {
-        console.log(error.message);
-        NextResponse.json({ message: error, status: 500 });
-    }
-}
+    return NextResponse.json(cardInfo);
+  } catch (error: any) {
+    console.error(error.message);
+    return NextResponse.json({ message: error.message, status: 500 });
+  }
+};
