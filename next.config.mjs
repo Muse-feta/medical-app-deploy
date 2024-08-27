3bc1d259ae0f427e5d3cb8bff5c0f12a384e4b5a
Muse-feta <fetamuse@gmail.com>
2024-08-27 23:24:47 +0300

    minor

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["api.dicebear.com"], // Add any other domains you need
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
   // Other configurations...
 };
 
